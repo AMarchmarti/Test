f99c7db370fe56c8fad01832ad0b7f2aac84f1da
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 15:55:47 +0100

    creacion de los casos test

---
+++ src/test/java/org/htlinking/LoginTest.java
@@ -0,0 +1,16 @@
+package org.htlinking;
+
+import org.htlinking.Users.Login;
+import org.htlinking.Users.Register;
+import org.htlinking.Users.User;
+import org.junit.Before;
+
+public class LoginTest {
+
+    private Login login = null;
+
+    @Before
+    public void __init__() {
+        login = new Login();
+    }
+}
