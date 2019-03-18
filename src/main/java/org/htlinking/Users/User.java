6cd4992735eeb49411f097e26ee58c420907b175
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 15:09:02 +0100

    creacion de getters, setters y validacion

--- src/main/java/org/htlinking/Users/User.java
+++ src/main/java/org/htlinking/Users/User.java
@@ -5,30 +5,69 @@ import java.security.PublicKey;
 
 public class User {
 
-        private String name = null;
-        private String email = null;
-        private String password = null;
-        private PublicKey pubKey = null;
-        private PrivateKey priKey = null;
+    private String name = null;
+    private String email = null;
+    private String password = null;
+    private PublicKey pubKey = null;
+    private PrivateKey priKey = null;
 
-        //Constructor
-        public User(String name, String email, String password) {
-            this.name = name;
-            this.email = email;
-            this.password = password;
+    //Constructor
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    //Setters
+
+
+    public void setPubKey(PublicKey pubKey) {
+        this.pubKey = pubKey;
+    }
+
+    public void setPriKey(PrivateKey priKey) {
+        this.priKey = priKey;
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public PublicKey getPubKey() {
+        return pubKey;
+    }
+
+    public PrivateKey getPriKey() {
+        return priKey;
+    }
+
+    public void generateKeyPair() {
+        setPubKey(GenSig.generateKeyPair().getPublic());
+        setPriKey(GenSig.generateKeyPair().getPrivate());
+    }
+
+
+    public void registerUser(String pass, Register register){
+        register.register(this);
+        validateUser(pass);
+
+    }
+
+    public void validateUser(String password){
+        if (password == getPassword()){
+            System.out.println("Ha sido registrado correctamente, mire las ofertas.");
+            //ir a ofertas
+        }else{
+            System.out.println("Se ha equivocado de contraseña");
         }
-
-
-        //Getters
-        public String getName() {
-            return name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
+    }
 }
