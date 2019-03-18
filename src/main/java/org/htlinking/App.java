b997077516db37d420fe00c2e7d88ea691b6ab86
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 15:49:20 +0100

    Primera historia de usuario, registro

--- src/main/java/org/htlinking/App.java
+++ src/main/java/org/htlinking/App.java
@@ -1,13 +1,63 @@
 package org.htlinking;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+
+import org.htlinking.Users.Register;
+import org.htlinking.Users.User;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Register registro = new Register();
+        User user = new User();
+
+        /* Primera historia de usuario: Como Usuario que quiere adquirir ofertas deseo poder registrarme*/
+        Scanner scanner = new Scanner(System.in);
+        Boolean enter = false;
+        while (!enter){
+            System.out.println("Bienvenido a nuestra plataforma de adquisición de ofertas para fabulosos viajes, hoteles" +
+                    "entre otras experiencias. \n" +
+                    "¿Desea registrarse? Si o No");
+
+            String respuesta = scanner.nextLine();
+
+            if (respuesta.toLowerCase().equals("si")) {
+
+                System.out.println("Nombre:");
+                String name = scanner.nextLine();
+                user.setName(name);
+
+                System.out.println("Email:");
+                String email = scanner.nextLine();
+                user.setEmail(email);
+
+                System.out.println("Contraseña:");
+                String pass = scanner.nextLine();
+                user.setPassword(pass);
+
+                System.out.println("Repita Contraseña:");
+                String rpass = scanner.nextLine();
+
+
+                user.registerUser(rpass, registro);
+                enter = true;
+
+            } else if (respuesta.toLowerCase().equals("no")) {
+
+                System.out.println("¿Quiere loggear?");
+                String loginin = scanner.nextLine();
+
+                if (loginin.toLowerCase().equals("si")) {
+                    //login
+                    enter = true;
+                }
+                else {
+                    System.out.println("Pase un buen día, Adiós");
+                    break;
+                }
+            }
+
+
+        }
+}}
