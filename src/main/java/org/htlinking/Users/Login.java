8597dcf09602e59933137700564f5ea0a117443f
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 16:38:02 +0100

    Logica del login aplicada

--- src/main/java/org/htlinking/Users/Login.java
+++ src/main/java/org/htlinking/Users/Login.java
@@ -1,5 +1,25 @@
 package org.htlinking.Users;
 
-public class Login {
+public  class Login {
 
+    public static Boolean checkEmail(String email, User user){
+        if(email.equals(user.getEmail())){
+            return true;
+        }
+        return false;
+    }
+
+    public static  Boolean checkPass(String pass, User user){
+        if(pass.equals(user.getPassword())){
+            return true;
+        }
+        return false;
+    }
+
+    public static Boolean login(String email, String pass, User user){
+        if ((checkEmail(email,user)) && checkPass(pass, user)){
+            return true;
+        }
+        return false;
+    }
 }
