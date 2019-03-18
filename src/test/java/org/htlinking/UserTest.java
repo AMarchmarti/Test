a3c936a47c777568177d2a3b2f1bb1b25107944a
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 18:40:56 +0100

    test promo

--- src/test/java/org/htlinking/UserTest.java
+++ src/test/java/org/htlinking/UserTest.java
@@ -2,6 +2,7 @@ package org.htlinking;
 
 import static org.junit.Assert.*;
 
+import org.htlinking.Offers.Offers;
 import org.htlinking.Users.Login;
 import org.htlinking.Users.Register;
 import org.htlinking.Users.User;
@@ -49,4 +50,16 @@ public class UserTest
     public void loginTest(){
         regis.register(user);
         assertTrue(Login.login(user.getEmail(), user.getPassword(), user));}
+
+    @Test
+    public void promoTest(){
+        Offers offers = new Offers();
+        offers.setPrefix("VKG");
+        offers.setOferta("Fiordos y la Aurora boreal: \nCanjeando este cupón obtendrás un 30% de " +
+                "descuento en un viaje preparado para poder sentir en tus pieles la tierra de los vikingos. \n" +
+                "Con este viaje entra el vuelo, la estancia y además una visita guiada por las tierras del norte.  " +
+                "Abriguense bien!");
+        offers.addDataBase(offers.getPrefix(), offers.getOferta());
+        System.out.println(user.promocionalCode("VKG", offers));
+    }
 }
