d1394d67e61efe4cde69fcc3143d119410012e02
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 15:09:32 +0100

    creacion de register

--- src/main/java/org/htlinking/Users/Register.java
+++ src/main/java/org/htlinking/Users/Register.java
@@ -1,8 +1,25 @@
 package org.htlinking.Users;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Register {
 
-        private Map<Integer,User> information =null;
+        private Map<PrivateKey,User> information = new HashMap<>();
+
+        //Getters
+
+
+        public Map<PrivateKey, User> getInformation() {
+                return this.information;
+        }
+
+        public void register(User user){
+                PrivateKey key = user.getPriKey();
+                getInformation().putIfAbsent(key, user);
+        }
+
+
 }
