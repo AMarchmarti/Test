9408dcf05cbf86467851277c1c013b8f3c440282
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 13:35:21 +0100

    Encapsulamiento

--- src/main/java/org/htlinking/Users/User.java
+++ src/main/java/org/htlinking/Users/User.java
@@ -18,4 +18,17 @@ public class User {
             this.password = password;
         }
 
+
+        //Getters
+        public String getName() {
+            return name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
 }
