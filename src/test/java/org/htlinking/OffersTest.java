0ba60cb0fc8085e3786fff4c11a3d85e6a2bb7ec
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 17:10:47 +0100

    Testeando estructuras de datos

---
+++ src/test/java/org/htlinking/OffersTest.java
@@ -0,0 +1,35 @@
+package org.htlinking;
+
+import org.htlinking.Offers.Offers;
+import org.htlinking.Users.Register;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OffersTest {
+
+    private Offers offers = null;
+
+
+    @Before
+    public void __init__() {
+        offers = new Offers();
+    }
+
+
+    @Test
+    public void arrayTest(){
+        offers.setPrefix("VKG");
+        offers.setName("Fiordos y la Aurora boreal");
+        offers.setDescription("Canjeando este cupón obtendrás un 30% de " +
+                "descuento en un viaje preparado para poder sentir en tus pieles la tierra de los vikingos. \n" +
+                "Con este viaje entra el vuelo, la estancia y además una visita guiada por las tierras del norte.  " +
+                "Abriguense bien!");
+        offers.addOffers(offers.getName(),offers.getDescription() );
+        offers.addDataBase(offers.getPrefix(), offers.getOfertas());
+        assertNotNull(offers.getOfertas());
+        assertNotNull(offers.getDataBase());
+        System.out.println(offers.getDescription());
+    }
+}
