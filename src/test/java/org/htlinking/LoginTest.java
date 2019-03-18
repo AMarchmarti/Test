0ab7ff5bacd8dffd8720ec47d430d36afe437956
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 16:37:44 +0100

    aplicado el login

--- src/test/java/org/htlinking/LoginTest.java
+++ src/test/java/org/htlinking/LoginTest.java
@@ -4,13 +4,20 @@ import org.htlinking.Users.Login;
 import org.htlinking.Users.Register;
 import org.htlinking.Users.User;
 import org.junit.Before;
+import org.junit.Test;
 
 public class LoginTest {
 
-    private Login login = null;
+    private Register register = null;
 
     @Before
     public void __init__() {
-        login = new Login();
+        register = new Register();
+    }
+
+    @Test
+    public void loginTest(){
+
+        //Login.checkRegis()
     }
 }
